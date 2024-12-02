233384232b68310ce1098426c334a403ac51216f
Palash <ariful.islamk777@gmail.com>
2024-12-03 02:00:15 +0600

    linkedlist

---
+++ link_list.py
@@ -0,0 +1,34 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def print_list(self):
+        current = self.head
+
+        while current:
+            print(current.data)
+            current = current.next
+        print("None")
+
+linked_list = LinkedList()
+linked_list.append(5)
+linked_list.append(10)
+linked_list.append(20)
+linked_list.print_list()
